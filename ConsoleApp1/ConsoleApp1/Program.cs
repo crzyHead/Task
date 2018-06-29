64933005e815d225d7cd0a86c25776325d8ac73f
crzyHead <37449089+crzyHead@users.noreply.github.com>
2018-06-29 15:40:04 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using TestUnit;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variant;
+            Console.WriteLine("Choose variant:\n 1) Empty Launch;\n 2) Release Launch");
+            try
+            {
+                variant = int.Parse(Console.ReadLine());
+                if (variant == 1)
+                {
+                    Loader.Load();
+                    Console.ReadLine();
+                }
+                else if (variant == 2)
+                {
+                    string Url = @"https://www.postjobfree.com/job/dpf2mj/telecommute-developer-ok-java-manila-ncr";
+                    string FileName = "index.html";
+                    string Directory = @"C:\Users\Comp\source\repos\ConsoleApp1\ConsoleApp1\";
+                    Console.Clear();
+                    Loader.Load(Url, FileName, Directory);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Choose the existing variant!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
---
+++ TestTask/TestTask/Loader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace TestUnit
+{
+    public class Loader
+    {
+        public static void Load()
+        {
+            Console.WriteLine("Job Name: \nJob Description: "); // Is this a JoJo refference?
+        }
+        public static void Load(string Url, string FileName, string Directory)
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFile(Url, Directory + FileName);
+            FileStream file = new FileStream(Directory + FileName, FileMode.Open);
+            StreamReader reader = new StreamReader(file);
+            string pattern = @"<p>(?<val>.*?)<\/p>";
+            RegexOptions options = RegexOptions.Compiled | RegexOptions.Multiline;
+            Regex regex = new Regex(pattern, options);
+            Match match = regex.Match(reader.ReadToEnd());
+            string result = "";
+            while (match.Success)
+            {
+                result += match.Groups["val"].Value + "\n";
+                match = match.NextMatch();
+            }
+            Console.WriteLine("Description:");
+            Console.WriteLine(result);
+            reader.Close();
+            File.Delete(Directory + FileName);
+        }
+    }
+}
